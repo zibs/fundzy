e09f2f8581b4917fead7bb0324919b6b12d3e640
zibs <elizibin@gmail.com>
2016-02-17 13:39:45 -0800

    Write Tests for Sessions Controller. Add Sessions Controller with New Create Destroy actions. Add sessions view for new. Add routes for sessions :)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      session[:user_id] = @user.id
+      sign_in(@user)
       redirect_to root_path, flash: { success: "User Created" }
     else
       flash[:warning] = "Invalid Attributes... Refer to Errors Below"
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   root "campaigns#index"
   resources :campaigns
   resources :users, only: [:new, :create]
+
+  resources :sessions, only: [:new, :create] do
+    delete :destroy, on: :collection
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
